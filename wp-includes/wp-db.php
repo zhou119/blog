bf67767027fc7fad734e9a8aaf0ce5e3f114b2ab
zhou <abc119136@163.com>
2018-03-26 11:11:27 +0800

    复原
    啦啦啦

--- wp-includes/wp-db.php
+++ wp-includes/wp-db.php
@@ -1559,9 +1559,9 @@ class wpdb {
 			}
 		} else {
 			if ( WP_DEBUG ) {
-				$this->dbh = mysqli_connect( $this->dbhost, $this->dbuser, $this->dbpassword, $new_link, $client_flags );
+				$this->dbh = mysql_connect( $this->dbhost, $this->dbuser, $this->dbpassword, $new_link, $client_flags );
 			} else {
-				$this->dbh = @mysqli_connect( $this->dbhost, $this->dbuser, $this->dbpassword, $new_link, $client_flags );
+				$this->dbh = @mysql_connect( $this->dbhost, $this->dbuser, $this->dbpassword, $new_link, $client_flags );
 			}
 		}
 
